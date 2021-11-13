57d37c03e2ff6af624e0c8a3f70ed6d43fbca01b
João Pedro <80638211+joao-pedro01@users.noreply.github.com>
2021-11-13 20:01:58 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,23 +1,47 @@
 ﻿function Escolha()
 {
     
-    let Option = document.getElementById('option')
+    let Option = document.getElementById('option');
+    let Value = document.getElementById ('value').value;
 
-    switch (Option.value) 
+    if (Value == 0)
     {
-        case 'horror':
-            window.location.href = 'pages/terror.html'
-            break;
-        case 'drama':
-            window.location.href = 'pages/drama.html'
-            break;
-        case 'kids':
-            window.location.href = 'pages/infantil.html'
-            break;
-        case 'family':
-            window.location.href = 'pages/familia.html'
-            break;
-        default: alert ("Selecione uma opção válida!!!")
-            break;
+        switch (Option.value) 
+        {
+            case 'horror':
+                window.location.href = 'pages/terror.html'
+                break;
+            case 'drama':
+                window.location.href = 'pages/drama.html'
+                break;
+            case 'kids':
+                window.location.href = 'pages/infantil.html'
+                break;
+            case 'family':
+                window.location.href = 'pages/familia.html'
+                break;
+            default: alert ("Selecione uma opção válida!!!")
+                break;
+        }
+    }
+    else
+    {
+        switch (Option.value) 
+        {
+            case 'horror':
+                window.location.href = '../pages/terror.html'
+                break;
+            case 'drama':
+                window.location.href = '../pages/drama.html'
+                break;
+            case 'kids':
+                window.location.href = '../pages/infantil.html'
+                break;
+            case 'family':
+                window.location.href = '../pages/familia.html'
+                break;
+            default: alert ("Selecione uma opção válida!!!")
+                break;
+        }
     }
 }
