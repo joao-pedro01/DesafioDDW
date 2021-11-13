ed37c7055772e63dbc64f9e5f3750142f2c28504
João Pedro <80638211+joao-pedro01@users.noreply.github.com>
2021-11-13 18:35:06 -0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,23 @@
+﻿function Escolha()
+{
+    
+    let Option = document.getElementById('option')
+
+    switch (Option.value) 
+    {
+        case 'horror':
+            window.location.href = 'terror.html'
+            break;
+        case 'drama':
+            window.location.href = 'drama.html'
+            break;
+        case 'kids':
+            window.location.href = 'infantil.html'
+            break;
+        case 'family':
+            window.location.href = 'familia.html'
+            break;
+        default: alert ("Selecione uma opção válida!!!")
+            break;
+    }
+}
